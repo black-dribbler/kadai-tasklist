8e7274ddac324304fc08033b5754e39ac4c6a3d5
black.dribbler <black.dribbler@gmail.com>
2018-06-20 06:41:26 +0000

    add status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -46,9 +46,10 @@ class TasksController < ApplicationController
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end
-end
 
-#strong parameter
-def task_params
-  params.require(:task).permit(:content)
+  #strong parameter
+  def task_params
+   params.require(:task).permit(:content, :status)
+  end
+
 end
