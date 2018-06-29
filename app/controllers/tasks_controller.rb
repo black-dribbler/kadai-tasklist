2efb2457d98bfc044d643251b3fd85275d4e3c2a
black.dribbler <black.dribbler@gmail.com>
2018-06-29 08:18:51 +0000

    task list heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,10 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
+    if logged_in?
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page]).per(3)
+    end
   end
   
   def show
@@ -12,7 +16,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に登録されました'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+
+
   root to: 'tasks#index'
+  
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/create'
+  
+  get 'signup', to: 'users#new'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   resources :tasks
+  resources :users, only: [:show, :new, :create]
 end
