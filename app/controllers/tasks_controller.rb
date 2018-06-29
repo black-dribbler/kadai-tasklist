fba6b8ef57ee705e2eb2be38f7f7d7a453bf526d
black.dribbler <black.dribbler@gmail.com>
2018-06-29 10:50:31 +0000

    task-list-heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :show]
+  before_action :correct_user, only: [:show, :destroy, :edit, :update]
   
   def index
     if logged_in?
@@ -8,7 +9,8 @@ class TasksController < ApplicationController
   end
   
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    # @task = current_user.tasks.find_by(params[:id])
   end
   
   def new
@@ -28,11 +30,13 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    # @task = current_user.tasks.find_by(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
+    # @task = current_user.tasks.find_by(params[:id])
     
     if @task.update(task_params)
       flash[:success] = 'Taskは正常に更新されました'
@@ -44,16 +48,25 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.destroy
     
     flash[:success] = 'Taskは正常に削除されました'
     redirect_to tasks_url
   end
+  
+  private
 
   #strong parameter
   def task_params
    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 
 end
